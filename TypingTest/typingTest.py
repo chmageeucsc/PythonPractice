0a0056448b5f5c2b542e4b25dca230e122c33cf7
Chantel <chmagee@ucsc.edu>
2024-06-24 17:12:26 -0700

    apostrophe and brackets will end program early

--- TypingTest/typingTest.py
+++ TypingTest/typingTest.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -20,8 +21,13 @@ def display_text(stdscr, target, current, wpm=0):
             color = curses.color_pair(2)
         stdscr.addstr(0, i, char, color)
 
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
 def wpm_test(stdscr):
-    target_text = "Hello world. This is a tester text."
+    target_text = load_text()
     current_text = []
     wpm = 0
     start_time = time.time()

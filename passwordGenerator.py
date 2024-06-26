4db5f6b7ad15d2d4089d5ba9189fbab8213d3393
Chantel <chmagee@ucsc.edu>
2024-06-26 11:16:33 -0700

    used colon instead of equal sign for has_special

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,6 @@
 import random
 import string
+import time
 
 def generate_password(min_length, numbers=True, special_characters=True):
     letters = string.ascii_letters
@@ -22,18 +23,31 @@ def generate_password(min_length, numbers=True, special_characters=True):
         pwd += new_char
 
         if new_char in digits:
+            print("New digit is", new_char)
             has_number = True
         elif new_char in special:
-            has_special: True
+            print("New special is", new_char)
+            has_special = True
+            print("has special???", has_special, "\n\n")
+        else:
+            print("New letter is", new_char)
 
         meets_criteria = True
         if numbers:
             meets_criteria = has_number
         if special_characters:
             meets_criteria = meets_criteria and has_special
+
+        # tests
+        print("Password:", pwd)
+        print("has number:", has_number)
+        print("has special:", has_special)
+        print("has meets:", meets_criteria, "\n")
+        time.sleep(4)
+
     return pwd
 
-min_length = int(input("How long should the password be? "))
+min_length = int(input("Minimum length of password? "))
 has_number = input("Should the password contain numbers? [y/n]: ").lower() == "y"
 has_special = input("Should the password contain special characters? [y/n]: ").lower() == "y"
 

97ed7c74ae3b5cc1e278329790da0289f19d3475
Chantel <chmagee@ucsc.edu>
2024-06-28 12:46:37 -0700

    prints shortest path

--- shortestPath.py
+++ shortestPath.py
@@ -17,20 +17,77 @@ maze = [
 
 def print_maze(maze, stdscr, path=[]):
     BLUE = curses.color_pair(1)
-    RED = curses.color_pair(1)
+    RED = curses.color_pair(2)
 
     for i, row in enumerate(maze):
         for j,value in enumerate(row):
-            stdscr.addstr(i, j*2, value, BLUE)
+            if (i,j) in path:
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
+
+def find_start(maze, start):
+    # loop through all the rows and columns to find the "O" starting position
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+    return None
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+    # starting position and list with starting position in it
+    # keep track of current node and path to that node
+    q.put((start_pos, [start_pos]))
+    
+    # keep track of all nodes visited
+    visited = set()
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+
+        stdscr.clear()
+        print_maze(maze, stdscr, path)
+        time.sleep(0.2)
+        stdscr.refresh()
+
+        if maze[row][col] == end:
+            return path
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+    if row > 0: # above
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze): # below
+        neighbors.append((row + 1, col))
+    if col > 0: # left
+        neighbors.append((row, col -1))
+    if col + 1 < len(maze[0]): # right
+        neighbors.append((row, col + 1))
+
+    return neighbors
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    print_maze(maze, stdscr)
-    # stdscr.addstr(0,0, "hello world!", blue_and_black)
-    stdscr.refresh()
+    find_path(maze, stdscr)
     stdscr.getch()
 
 wrapper(main)

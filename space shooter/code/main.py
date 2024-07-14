7d32995a70a7d0d70aaf8db054ed5df3d92273b0
Chantel <chmagee@ucsc.edu>
2024-07-14 15:54:53 -0700

    set up for spawning meteor ever half second

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -52,12 +52,18 @@ meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
+# custom event - meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 500)
+
 while running:
     dt = clock.tick() / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == meteor_event:
+            print("create meteor")
 
     # update
     all_sprites.update(dt)

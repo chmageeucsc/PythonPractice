26ae0dfa665e0e14b2d5e54a5e9a4fd67d0fa710
Chantel <chmagee@ucsc.edu>
2024-07-19 12:04:29 -0700

    display score with font and draw

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -81,6 +81,13 @@ def collisions():
         if collided_sprites: 
             laser.kill()
 
+def dispay_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time), True, (240,240,240))
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH/ 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, 'white', text_rect.inflate(20,10).move(0,-8), 5, 10)
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -93,6 +100,7 @@ clock = pygame.time.Clock()
 star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
 meteor_surf = pygame.image.load(join('space shooter', 'images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).convert_alpha()
+font = pygame.font.Font(join('space shooter', 'images', 'Oxanium-Bold.ttf'), 40)
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -105,9 +113,7 @@ player = Player(all_sprites)
 # custom event - meteor event
 meteor_event = pygame.event.custom_type()
 pygame.time.set_timer(meteor_event, 500)
-
-test_rect = pygame.FRect(0, 0, 300, 600)
-
+ 
 while running:
     dt = clock.tick() / 1000
     # event loop
@@ -123,11 +129,10 @@ while running:
     collisions()
 
     # draw the game
-    display_surface.fill('darkblue')
+    display_surface.fill('#3a2e3f')
     all_sprites.draw(display_surface)
 
-    # test collision
-    player.rect.colliderect(test_rect)
+    dispay_score()
 
     pygame.display.update()
 

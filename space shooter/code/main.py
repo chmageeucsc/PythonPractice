0ed801ba05e5d55fb0ded298ec99213ac28597fd
Chantel <chmagee@ucsc.edu>
2024-07-19 13:58:11 -0700

    meteor rotation

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -64,12 +64,16 @@ class Laser(pygame.sprite.Sprite):
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, pos, groups):
         super().__init__(groups)
+        self.original_surf = surf
         self.image = surf
         self.rect = self.image.get_frect(center = pos)
         self.start_time = pygame.time.get_ticks()
         self.lifetime = 2500
         self.direction = pygame.Vector2(uniform(-0.5, 0.5), 1)
         self.speed = randint(400,500)
+
+        self.rotation_speed = randint(40,80)
+        self.rotation = 0
 
         # mask
         # self.mask = pygame.mask.from_surface(self.image)
@@ -78,6 +82,11 @@ class Meteor(pygame.sprite.Sprite):
         self.rect.center += self.direction * self.speed * dt
         if pygame.time.get_ticks() - self.start_time >= self.lifetime:
             self.kill()
+        
+        # continuous transformation
+        self.rotation += self.rotation_speed * dt
+        self.image = pygame.transform.rotozoom(self.original_surf, self.rotation, 1)
+        self.rect = self.image.get_frect(center = self.rect.center)
 
 def collisions():
     global running

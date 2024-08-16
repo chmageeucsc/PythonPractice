1cb5f4662c9c08ea647a0668c4305ae6aa9049c9
Chantel <chmagee@ucsc.edu>
2024-08-16 16:21:11 -0700

    player class

---
+++ Vampire survivor/code/player.py
@@ -0,0 +1,8 @@
+from pygame.sprite import _Group
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('Vampire survivor', 'images', 'player', 'down', '0.png'))
+        self.rect = self.image.get_frect(center = pos)

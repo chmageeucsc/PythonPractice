7349e20afeb67923476dff68e73995bdcaff033e
Chantel <chmagee@ucsc.edu>
2024-08-16 17:08:25 -0700

    sprites setup

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,12 +1,20 @@
 from settings import *
+from player import Player
 
 class Game: 
     def __init__(self):
+        # setup 
         pygame.init()
         self.display_surfface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Survivor")
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprites.Group()
+
+        # sprites
+        self.player = Player((400, 300), self.all_sprites)
 
     def run(self):
         while self.running:
